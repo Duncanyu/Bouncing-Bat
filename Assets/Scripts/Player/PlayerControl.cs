80cfebca8b3a45a205a3de2ac4593c98f0bcb409
Duncanyu <duncan.w.yu@gmail.com>
2025-02-11 11:01:46 -0500

    Player Movement Completed
    PlayerControl.cs

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D player_rigidbody;
+
+    public int jump_power = 10;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) == true){
+            player_rigidbody.velocity = new Vector2(player_rigidbody.velocity.x, jump_power);
+        }
     }
 }
